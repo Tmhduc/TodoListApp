e1dc3b6e144eb86361f93ff44db2c6958f43fe58
Tmhduc <duc_ta@sfu.ca>
2024-06-19 22:59:38 -0700

    First commit

---
+++ src/main/java/todolist/app/ductodo/DuctodoApplication.java
@@ -0,0 +1,13 @@
+package todolist.app.ductodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuctodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuctodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/todolist/app/ductodo/DuctodoApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist.app.ductodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuctodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
