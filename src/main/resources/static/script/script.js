31cc70fc96bfdd6ef09f40e9c241badf7ec7a422
Tmhduc <duc_ta@sfu.ca>
2024-07-19 12:37:19 -0700

    feat: remove and add button working

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,22 +1,62 @@
 //Adding a deleting button to all the tasks
 var myTaskList = document.getElementsByTagName("li");
 for (var i = 0; i < myTaskList.length; i++){
+
+
+}
+
+function addRemoveButton(taskElement){
     var span = document.createElement("button");
     var text = document.createTextNode("Remove");
     span.className = "remove";
     span.appendChild(text);
-    myTaskList[i].appendChild(span);
-}
+    taskElement.appendChild(span);
 
-//Click on the delete button to remove the task
-var close = document.getElementsByClassName("remove");
-for (var i = 0; i < close.length; i++){ //iterating through the close button list
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.display = 'none';
+    span.onclick = function(){
+        var currentElement = this.parentElement;
+        currentElement.remove();
     }
 }
 
-//Adding a checked symbol when clicking on a task
+function addCheckedToggle(taskElement){
+    taskElement.onclick = function(){
+        taskElement.classList.toggle("checked");
+    }
+}
+//Adding task
+document.getElementById("button").onclick = function(){newTask()}
 
+// Adding task when the Enter key is pressed
+document.getElementById("input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Prevent default form submission
+        newTask();
+    }
+});
 
+function newTask(){
+    const Tasks = document.getElementById("myTask");
+    const newtask = document.getElementById("input").value;
+
+    var li = document.createElement("li");
+    var t = document.createTextNode(newtask);
+    li.setAttribute("id", "task")
+    li.appendChild(t);
+
+    if(newtask === ''){
+        alert("The input field is empty!!!");
+    }
+    else {
+        Tasks.appendChild(li);
+    }
+
+    //resetting the input field
+    var inputBox = document.getElementById("input");
+    inputBox.value = "";
+
+    addRemoveButton(li);
+
+    addCheckedToggle(li);
+}
+
+//Cross out the task when clicked on
